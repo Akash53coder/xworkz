43baed078f04980d9eeba27bcea312ffb3c8dff2
akash m <akash.mabali7@gmail.com>
2021-07-28 13:37:05 +0530

    modified sysout statement

--- JavaIf.java
+++ JavaIf.java
@@ -2,7 +2,7 @@ class JavaIf{
     public static void main(String args[]){
         int groomAge=23;
         if(groomAge>=23){
-            System.out.println("He can marry");
+            System.out.println("He can marry at this age");
         }
     }
 }
