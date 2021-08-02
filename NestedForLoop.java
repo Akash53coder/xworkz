4429e71e87a06756af7c4bcd2afb5b628e8afa09
akash m <akash.mabali7@gmail.com>
2021-08-02 10:37:44 +0530

    Java code of Nested For loop to print pattern and prime numers between 1 to 50

--- NestedForLoop.java
+++ NestedForLoop.java
@@ -13,19 +13,15 @@ class NestedForLoop{
             System.out.println();
         }
 
- /* to print patter 1 2 3 4  
-                    5 6 7 8  */
+ /* to print pattern 1 2 3 4  
+                     5 6 7 8  */
         System.out.println("-------------------------------------------");                        
-
-            for(int i=1;i<=8;i++){                
-                System.out.print(i);
-                for(int j=1;j<=1;j++){
-                    System.out.print(" ");
-                }
-                if(i%4==0){
-                    System.out.println();
-                }
-                
+            int ct=1;
+            for(int i=1;i<=2;i++){                
+                 for(int j=1;j<=4;j++){
+                   System.out.print((ct++)+" ");
+               }
+               System.out.println();                
             }
 
 /* For printing Prime numbers 1 to 50 */
@@ -46,9 +42,9 @@ class NestedForLoop{
         /* for printing pattern of * * * *
                                    * * * * 
                                    * * * *
-                                   * * * *      */
+                                   * * * *     */
         System.out.println("-------------------------------------------");                        
-        for(int i=1;i<=3;i++){
+        for(int i=1;i<=4;i++){
             for(int j=1;j<=4;j++){
                 System.out.print("*"+" ");
             }
@@ -58,13 +54,4 @@ class NestedForLoop{
 
     }
 }
-
-
-
-
-
-
-
-
-
 
