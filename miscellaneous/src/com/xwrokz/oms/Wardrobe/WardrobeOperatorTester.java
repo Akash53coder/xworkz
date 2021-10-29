7a96673c2e64eb4ded6d435155cf9832cab50e23
akash m <akash.mabali7@gmail.com>
2021-10-29 08:24:55 +0530

    Enhanced the Code WardrobeOperator

--- miscellaneous/src/com/xwrokz/oms/Wardrobe/WardrobeOperatorTester.java
+++ miscellaneous/src/com/xwrokz/oms/Wardrobe/WardrobeOperatorTester.java
@@ -3,21 +3,66 @@ package com.xwrokz.oms.Wardrobe;
 public class WardrobeOperatorTester {
 	public static void main(String[] args) {
 		WardrobeOperator wdrOp = new WardrobeOperator();
-		wdrOp.add("t-shirt");
-		wdrOp.add("shirt");
-		wdrOp.add("pant");
-		wdrOp.add("hanger");
-		wdrOp.add("sandals");
-		
-		wdrOp.update(2, "shoes");
-		
-		wdrOp.delete(4);
-		
-		wdrOp.match("sandals");
-		wdrOp.matchFirstChar("p");
-		wdrOp.matchLastChar("r");
-		
-		
+		if (wdrOp.add("t-shirt")) {
+			System.out.println("thing added");
+		} else {
+			System.out.println("thing not added");
+		}
+
+		if (wdrOp.add("shirt")) {
+			System.out.println("thing added");
+		} else {
+			System.out.println("thing not added");
+		}
+
+		if (wdrOp.add("pant")) {
+			System.out.println("thing added");
+		} else {
+			System.out.println("thing not added");
+		}
+
+		if (wdrOp.add("hanger")) {
+			System.out.println("thing added");
+		} else {
+			System.out.println("thing not added");
+		}
+
+		if (wdrOp.add("sandals")) {
+			System.out.println("thing added");
+		} else {
+			System.out.println("thing not added");
+		}
+
+		if (wdrOp.update(2, "shoes")) {
+			System.out.println("thing updated");
+		} else {
+			System.out.println("thing not updated");
+		}
+
+		if (wdrOp.delete(4)) {
+			System.out.println("thing deleted");
+		} else {
+			System.out.println("thing not deleted");
+		}
+
+		if (wdrOp.match("sandals")) {
+			System.out.println("thing matched");
+		} else {
+			System.out.println("thing not matched");
+		}
+
+		if (wdrOp.matchFirstChar("p")) {
+			System.out.println("thing matched");
+		} else {
+			System.out.println("thing not matched");
+		}
+
+		if (wdrOp.matchLastChar("r")) {
+			System.out.println("thing matched");
+		} else {
+			System.out.println("thing not matched");
+		}
+
 	}
-	
+
 }
